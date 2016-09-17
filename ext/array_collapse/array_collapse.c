487decf775f9395a55c1caf9889997c366e597f8
Alex Moore-Niemi <moore.niemi@gmail.com>
2016-09-17 14:56:57 -0400

    weird indents

--- ext/array_collapse/array_collapse.c
+++ ext/array_collapse/array_collapse.c
@@ -3,45 +3,45 @@
 void Init_array_collapse();
 
 VALUE method_collapse(VALUE self, VALUE args) {
-	long len = RARRAY_LEN(self);
-	VALUE result = rb_ary_new2(len);
-	VALUE stack = rb_ary_new();
+  long len = RARRAY_LEN(self);
+  VALUE result = rb_ary_new2(len);
+  VALUE stack = rb_ary_new();
 
 #ifdef LOGGING
-	FILE *f = fopen("clog.txt", "w");
-	if (f == NULL) {
-		printf("no log file found\n");
-		exit(1);
-	}
-	fprintf(f, "len: %lu\n", len);
-	fprintf(f, "array: %s\n", RSTRING_PTR(rb_ary_to_s(self)));
+  FILE *f = fopen("clog.txt", "w");
+  if (f == NULL) {
+    printf("no log file found\n");
+    exit(1);
+  }
+  fprintf(f, "len: %lu\n", len);
+  fprintf(f, "array: %s\n", RSTRING_PTR(rb_ary_to_s(self)));
 #endif
 
-	rb_ary_push(stack,self);
-	while(RARRAY_LEN(stack) > 0) {
-		VALUE array = rb_ary_pop(stack);
-		long i;
-		for(i = 0; i < RARRAY_LEN(array); i++) {
-			VALUE e = RARRAY_AREF(array,i);
-			switch (TYPE(e)) {
-				case T_ARRAY:
-					rb_ary_push(stack, e);
-					break;
-				default:
-					if (rb_block_given_p()) {
-						e = rb_yield(e);
-					}
+  rb_ary_push(stack,self);
+  while(RARRAY_LEN(stack) > 0) {
+    VALUE array = rb_ary_pop(stack);
+    long i;
+    for(i = 0; i < RARRAY_LEN(array); i++) {
+      VALUE e = RARRAY_AREF(array,i);
+      switch (TYPE(e)) {
+        case T_ARRAY:
+          rb_ary_push(stack, e);
+          break;
+        default:
+          if (rb_block_given_p()) {
+            e = rb_yield(e);
+          }
           if (!NIL_P(e)) {
             rb_ary_push(result, e);
           }
-					break;
-			}
-		}
-	}
+          break;
+      }
+    }
+  }
 
-	return result;
+  return result;
 }
 
 void Init_array_collapse() {
-	rb_define_method(rb_cArray, "collapse", method_collapse, -2);
+  rb_define_method(rb_cArray, "collapse", method_collapse, -2);
 }
